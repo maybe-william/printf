5e16a66da707e6674f73409e4cc45aebfe3ce886
Danny Hollman <dannyhollman@gmail.com>
2019-07-27 16:00:54 +0000

    [LOCAL][INIT] Add file o_format

---
+++ o_format.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  *
+  *
+  *
+  */
+void get_oct(int n)
+{
+	int temp = 0;
+
+	if (n != 0)
+	{
+		temp = n % 8;
+		get_oct(n / 8);
+		_putchar(temp + 48);
+	}
+}
+int o_format(char *format, int n)
+{
+	get_oct(n);
+
+	return (2);
+}
