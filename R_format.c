2bd5361cd0e871639a3ee0dc0dcb3ce902549749
william <818@holbertonschool.com>
2019-07-27 15:28:45 +0000

    [LOCAL][INIT] Add R_format()

---
+++ R_format.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+void rec_r_form(char *);
+
+/**
+ * R_format - print a string in ROT13
+ * @spec: the spec string
+ * @data: the string to print
+ * Return: the length of the spec string (to skip over it)
+ */
+int R_format(char *spec, char *data)
+{
+	char *s1;
+	int i, printed;
+	char *reg;
+	char *lt;
+
+	s1 = data;
+	reg = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	lt = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*s1 != '\0')
+	{
+		printed = 0;
+		for (i = 0; reg[i] != '\0'; i++)
+		{
+			if (*s1 == reg[i])
+			{
+				_putchar(lt[i]);
+				printed = 1;
+				break;
+			}
+		}
+		if (!printed)
+			_putchar(*s1);
+		s1 = s1 + 1;
+	}
+	/*The default spec length is 2*/
+	return (2);
+}
