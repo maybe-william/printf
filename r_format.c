0ae309015c76fcf14a5f5b030d8018199b4eb612
william <818@holbertonschool.com>
2019-07-27 12:04:30 +0000

    [LOCAL][UPDATE] Fix betty style

--- r_format.c
+++ r_format.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
 
+void rec_r_form(char *);
 
+/**
+ * r_format - print a string in reverse
+ * @spec: the spec string
+ * @data: the string to print
+ * Return: the length of the spec string (to skip over it)
+ */
 int r_format(char *spec, char *data)
 {
 	char *tmp = spec;
@@ -10,7 +17,10 @@ int r_format(char *spec, char *data)
 	return (2);
 }
 
-
+/**
+ * rec_r_form - recursively print the string in reverse
+ * @data: the string to print
+ */
 void rec_r_form(char *data)
 {
 	if (*data == '\0')
