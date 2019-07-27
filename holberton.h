7755ae5c6ba8c9d59f4b01b94173a878f0c833be
william <818@holbertonschool.com>
2019-07-27 12:49:51 +0000

    [LOCAL][UPDATE] Add all format prototypes

---
+++ holberton.h
@@ -0,0 +1,36 @@
+
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int di_format(char *, int);
+
+int o_format(char *, unsigned int);
+
+int u_format(char *, unsigned int);
+
+int x_format(char *, unsigned int);
+
+int X_format(char *, unsigned int);
+
+int c_format(char *, char);
+
+int s_format(char *, char*);
+
+int p_format(char *, void *);
+
+int S_format(char *, char *);
+
+int r_format(char *, char *);
+
+int R_format(char *, char *);
+
+int b_format(char *, unsigned int);
+
+int _printf(const char *, ...);
+
+#endif
