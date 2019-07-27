b549af9929b7fc94ad18183991a51c5205261a8d
william <818@holbertonschool.com>
2019-07-27 12:04:02 +0000

    [LOCAL][UPDATE] Fix betty style

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,11 @@
 #include "holberton.h"
 #include <unistd.h>
 
+/**
+ * _putchar - put a char
+ * @c: the char to put
+ * Return: 0 if kosher, -1 if problems
+ */
 int _putchar(char c)
 {
 	write(1, &c, 1);
